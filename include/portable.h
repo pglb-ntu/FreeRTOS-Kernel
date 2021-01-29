e52340a7b98b4df388508c723d3ba3a30a68cee8
Hesham Almatary <Hesham.Almatary@cl.cam.ac.uk>
2021-01-29 13:12:06 +0200

    compartmentalization: Add per-compartment setters/getters for stack/return caps

--- include/portable.h
+++ include/portable.h
@@ -209,6 +209,8 @@ void vPortEndScheduler( void ) PRIVILEGED_FUNCTION;
 
 #if ( portHAS_COMPARTMENT == 1 )
     BaseType_t xPortGetCurrentCompartmentID( void ) PRIVILEGED_FUNCTION;
+    uintcap_t xPortGetCurrentCompartmentStack( void ) PRIVILEGED_FUNCTION;
+    uintcap_t xPortGetCurrentCompartmentReturn( void ) PRIVILEGED_FUNCTION;
     xCOMPARTMENT_RET xPortCompartmentEnter( BaseType_t ( * pxFunction ) ( void ), void *pxData, xCOMPARTMENT_ARGS *pxArgs, BaseType_t xCompID ) PRIVILEGED_FUNCTION;
 #endif
 
